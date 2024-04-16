b36342a58223743fd65496219563e7f889c7275a
jdjoum <jdjoum@hotmail.com>
2024-04-16 16:27:04 -0400

    finish implementation and add text file

--- wpm.py
+++ wpm.py
@@ -1,5 +1,7 @@
 import curses
 from curses import wrapper
+import time
+import random
 
 # start_screen - Displays the welcome screen before the game starts
 def start_screen(stdscr):
@@ -9,36 +11,53 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
-# display_text - 
+# display_text - Displays the target and input text and the wpm value
 def display_text(stdscr, target_text, current_text, wpm=0):
     stdscr.addstr(target_text)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
 
     # enumerate for loop to get the value and index
     for i, char in enumerate(current_text):
-        correct_char = target[i]
+        correct_char = target_text[i]
         color = curses.color_pair(1)
         if char != correct_char:
             color = curses.color_pair(2)
-        stdscr.addstr(0, i, char, curses.color_pair(1))
+        stdscr.addstr(0, i, char, color)
+
+# load_text - Loads the text samples in from the text file
+def load_text():
+    with open("text.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+
+
 
 
 # wpm_test - Handles the logic for playing the WPM Game
 def wpm_test(stdscr):
-    target_text = "Hello world this is some test text for this app!"
+    target_text = load_text()
     current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
         stdscr.clear()
-        display_text(stdscr, target_text, current_text)
-        stdscr.refresh()
-        # stdscr.addstr(target_text)
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
-
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
-        key = stdscr.getkey()
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break    
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+
         # Escape the program with the escape character
         if ord(key) == 27:
             break
@@ -46,7 +65,7 @@ def wpm_test(stdscr):
         if key in ("KEY_BACKSPACE", '\b', "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
-        else:
+        elif len(current_text) < len(target_text):
             current_text.append(key)
 
 # main - The start of the WPM Game program
@@ -56,7 +75,12 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text! Press any key to continue")
+        key = stdscr.getkey()
+        if ord(key) == 27:
+            break
 
 wrapper(main)
 
